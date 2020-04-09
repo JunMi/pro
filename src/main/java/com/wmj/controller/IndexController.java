be2238c20e67d68343a9d8b25612f69318f31373
wumj <648326357@qq.com>
2020-04-09 14:57:53 +0800

    add first

---
+++ src/main/java/com/wmj/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.wmj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/welcome.do", method = RequestMethod.GET)
+    public ModelAndView welcome() {
+        ModelAndView model = new ModelAndView("welcome");
+        System.out.print("welcome");
+        model.addObject("wmj","芜湖文化");
+        return model;
+    }
+
+    @RequestMapping("index.do")
+    public ModelAndView index() {
+        ModelAndView model = new ModelAndView("index");
+        return model;
+    }
+}
---
+++ src/main/webapp/index1111.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%
+    String path = request.getContextPath();
+    String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+%>
+<html>
+<body>
+<script type="text/javascript">
+    window.location.href='<%=basePath%>index.do';
+</script>
+</body>
+</html>
